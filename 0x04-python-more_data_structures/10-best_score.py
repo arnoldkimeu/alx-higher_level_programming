2e0f2abee8acf60f1367775667ca753dda291b5f
arnoldkimeu <arnoldkimeu@gmail.com>
2022-12-07 13:19:57 -0800

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    result = 0
-    if a_dictionary:
-        for key in a_dictionary:
-            if a_dictionary[key] > result:
-                result = a_dictionary[key]
-                winner = key
-                return winner
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
